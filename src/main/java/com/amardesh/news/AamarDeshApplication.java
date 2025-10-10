3d29b09d642cf47b50aa8016b20fbfaae20b5fbb
Md. Abir Hossain <dreamshadow.ab@gmail.com>
2025-10-10 16:55:47 +0600

    initial commit

---
+++ src/main/java/com/amardesh/news/AamarDeshApplication.java
@@ -0,0 +1,13 @@
+package com.amardesh.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AamarDeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AamarDeshApplication.class, args);
+	}
+
+}
